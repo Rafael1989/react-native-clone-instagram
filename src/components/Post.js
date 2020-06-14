56a2caa654a8773a208875a3de382232253de074
unknown <flns_rafa@hotmail.com>
2020-06-14 23:21:25 +0100

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {StyleSheet, View, Image, Dimensions} from 'react-native'
+import Autor from './Autor'
+import Comments from './Comments'
+import AddComment from './AddComponent'
+
+class Post extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <Image source={this.props.image} style={styles.image} />
+                <Autor email='fulano@gmail.com' nickname='Fulano de Tal' />
+                <Comments comments={this.props.comments} />
+                <AddComment />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3/4,
+        resizeMode: 'contain'
+    }
+})
+
+export default Post
